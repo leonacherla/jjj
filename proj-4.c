db170edaf41a05a4033c8d8e635b6ecd57534e0f
leonacherla <leona@Leos-MacBook-Pro.local>
2015-04-22 06:08:33 -0700

    ad

--- proj-4.c
+++ proj-4.c
@@ -1,116 +1,144 @@
-//Sameeksha Cherla.
-
 #include "sem.h"
-#define READER 3
-#define WRITER 2
 
 
-TCB_t *runQ = 0;
-Sem *mx = 0;
-Sem *readerSemaphore = 0;
-Sem *writerSemaphore = 0;
-int x = 0;
-int readerCount = 0;
-int y = 0;
-int writerCount = 0;
-int value = 1;
+struct Q* RunQ;
+int global = 0;
 
+Sem* rsem; //<---0
+Sem* wsem; //<---0
+Sem* mutex; //<---1
+
+int rc = 0; //reader count
+int rwc = 0; //reader waiting count
+int wwc = 0; //writer waiting count
+int wc = 0; //write count
+int test[3] = {1,2,3};
+int read = 1;
+int write = 1;
+
+/********************
+ * Function Prototypes
+ ********************/
 void reader();
-void reader_start();
-void reader_end();
-
 void writer();
-void writer_start();
-void writer_end();
 
-void main() {
-TCB_t* threads[5]; 
-mx = malloc(sizeof(Sem)); 
-readerSemaphore = malloc(sizeof(Sem)); 
-writerSemaphore = malloc(sizeof(Sem)); 
-initSem(mx, 1); initSem(readerSemaphore, 0); 
-initSem(writerSemaphore, 0); 
-int iteratorA=0;
-int iteratorB=0; 
-int iteratorC=0;
-while (iteratorA < READER + WRITER) {
-if (iteratorB < READER ) {
-start_thread(reader);
-iteratorA++; iteratorB++;
+/*************************
+ * Function Pointers
+************************/
+void (*r1)();
+void (*r2)();
+void (*r3)();
+void (*w1)();
+void (*w2)();
+
+int main()
+{	
+	RunQ = new_Queue();
+
+	mutex = (Sem*)malloc(sizeof(Sem));
+	mutex->queue = new_Queue();
+	initSem(mutex, 1);
+
+	rsem = (Sem*)malloc(sizeof(Sem));
+	rsem->queue = new_Queue();
+	initSem(rsem,0);
+
+	wsem = (Sem*)malloc(sizeof(Sem));
+	wsem->queue = new_Queue();
+	initSem(wsem,0);
+
+	r1 = reader;
+	r2 = reader;
+	r3 = reader;
+	w1 = writer;
+	w2 = writer;
+
+	/*************************
+	 * Initialize Threads
+	 *************************/
+	start_thread(r1);
+	start_thread(w1);
+	start_thread(w2);
+	start_thread(r2);
+	start_thread(r3);
+
+	run(); //starts the first thread
+	return 0;
 }
-if (iteratorC < WRITER) {
-start_thread(writer);
-iteratorB++; iteratorC++;
-}}
-puts("\nstarting threads\n");
-run();
+
+void reader()
+{
+ printf("----Reader %d: %p----\n", read, RunQ->head);
+ read++;
+	
+ while(1){
+	// Reader enter
+	P(mutex);	
+	if(wwc>0 || wc>0) {
+		rwc++;
+		V(mutex);
+		P(rsem);
+		rwc--;
+	}
+	
+	rc++;
+	if(rwc>0)
+		V(rsem);
+	else
+		V(mutex);
+
+
+	//CS - read occurs here
+	printf("Reader %p\n", RunQ->head);
+	printf("Reading array: %d %d %d\n", test[0], test[1], test[2]);
+
+	// Reader exit
+	P(mutex);
+	rc--;
+	if(rc==0 && wwc>0)
+		V(wsem);
+	else
+		V(mutex);
+ }	
 }
-void reader() {
-while (1 > 0) {
-reader_start();
-printf("Reader started");
-P(mx);
-printf("Reader : %d\n", value);
-sleep(1);
-V(mx);
-printf("Reader left");
-reader_end();
-}
-}
-void reader_start() {
-P(mx);
-if (y > 0 || writerCount > 0) {
-x++;
-V(mx);
-P(readerSemaphore);
-x--;
-}
-readerCount++;
-if (x > 0) {
-V(readerSemaphore);
-} else {
-V(mx);
-}
-}
-void reader_end() {
-P(mx);
-readerCount--;
-if (readerCount == 0 && y > 0) {
-V(writerSemaphore);
-} else {
-V(mx);
-}
-}
-void writer() {
-while (1 > 0) {
-writer_start();
-printf("Writer started");
-P(mx);
-value = 2*value;
-sleep(1);
-V(mx);
-printf("Writer left");
-writer_end();
-}
-}
-void writer_start() {
-P(mx);
-if (readerCount > 0 || writerCount > 0) {
-y++;
-V(mx);
-P(writerSemaphore);
-y--;
-}
-writerCount++;
-V(mx);
-}
-void writer_end() {
-P(mx);
-writerCount--;
-if (x > 0) {
-V(readerSemaphore);
-}
-else if (y > 0) {
-V(writerSemaphore);
-}
+
+void writer()
+{
+ int i;
+ printf("----Writer %d: %p----\n", write, RunQ->head);
+ write++;
+
+  while(1) {
+	// Writer enter
+	P(mutex);
+	if(rc>0 || wc>0) {
+		wwc++;
+		V(mutex);
+		P(wsem);
+		wwc--;
+	}
+	
+	wc++;
+	V(mutex);
+
+
+	//CS - write occurs here
+	printf("Writer %p\n", RunQ->head);
+	for (i = 0; i < 3; i++) {
+		test[i] = test[i] * 4;
+	}
+	printf("Writing array: %d %d %d\n", test[0], test[1], test[2]);
+
+	// Writer exit
+	P(mutex);
+	wc--;
+	if(rwc>0)
+		V(rsem);
+	else {
+		if(wwc>0)
+			V(wsem);
+		else
+			V(mutex);
+	}
+ }
 }
